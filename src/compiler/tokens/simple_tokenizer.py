23fe7dd6e5bb1c5f87b3a4a6f9826745e320ffe7
Nick Manos <nmanos@student.bridgew.edu>
2024-04-26 02:08:07 -0400

    Refactor Token to use proper classes and subclasses

---
+++ src/compiler/tokens/simple_tokenizer.py
@@ -0,0 +1,86 @@
+#from typing import Literal, NamedTuple
+
+from compiler.tokens.simple_tokens import (
+    Precedence,
+    TokenBase,
+    Operator,
+    PrefixOperator,
+    InfixOperator,
+    LeftParen,
+    RightParen,
+    Mult,
+    Div,
+    Plus,
+    Minus,
+    Number
+)
+
+# type TokenType = Literal['NUMB', 'PLUS', 'MINUS', 'MULT', 'DIV', 'LPAREN', 'RPAREN']
+
+# class Token(NamedTuple):
+#     value: str
+#     type: TokenType
+    
+#     @property
+#     def precedence(self) -> int:
+#         match self.type:
+#             case 'LPAREN' | 'RPAREN':
+#                 return 3
+#             case 'MULT' | 'DIV':
+#                 return 2
+#             case 'PLUS' | 'MINUS':
+#                 return 1
+#             case _:
+#                 return -1
+
+#     # def __repr__(self):
+#         # return f'{self.value}\t{self.type}'
+#         # return f"Token({self.value}, '{self.type}')"
+    
+#     def __str__(self) -> str:
+#         return self.value
+
+class TokenizeError(Exception):
+    pass
+
+def tokenize(srcCode: str) -> list[TokenBase]:
+    tokenize_list: list[TokenBase] = [] # Initialize empty list
+
+    for c in srcCode:
+        match c:
+            case c if c.isdigit():
+                if len(tokenize_list) > 0 and isinstance(tokenize_list[-1], Number):
+                    tokenize_list[-1].value += c
+                else:
+                    tokenize_list.append(Number(c))
+            case '+':
+                tokenize_list.append(Plus())
+            case '-':
+                tokenize_list.append(Minus())
+            case '*':
+                tokenize_list.append(Mult())
+            case '/':
+                tokenize_list.append(Div())
+            case '(':
+                tokenize_list.append(LeftParen())
+            case ')':
+                tokenize_list.append(RightParen())
+            case c if c.isspace():
+                continue
+            case _:
+                # If c doesn't match any of these cases, raise an error
+                raise TokenizeError(f'Unknown token {c} in {srcCode}')
+
+    return tokenize_list
+
+if __name__ == '__main__':
+    __package__ = __package__ or 'compiler'
+
+    test_src = '(5 + 2) * (60 / 15) - 10'
+
+    tokens = tokenize(test_src)
+
+    print(test_src)
+
+    for token in tokens:
+        print(repr(token))
--- tests/test_simple_parser.py
+++ tests/test_simple_parser.py
@@ -1,221 +1,224 @@
+# ruff: noqa: F403
+# ruff: noqa: F405
+
 from compiler.binarytree import TreeNode
-from compiler.simple_tokenizer import Token
+from compiler.tokens.simple_tokens import *
 from compiler.simple_parser import parse
 
 
 class TestSimpleParser:
     def test_simple_src_no_reorder(self):
         test_src_tokens = [
-            Token(value="1", type="NUMB"),
-            Token(value="+", type="PLUS"),
-            Token(value="3", type="NUMB"),
-            Token(value="*", type="MULT"),
-            Token(value="4", type="NUMB"),
+            Number('1'),
+            Plus(),
+            Number('3'),
+            Mult(),
+            Number('4'),
         ]
 
-        expected_tree = TreeNode(Token(value="+", type="PLUS"))
-        expected_tree.left = TreeNode(Token(value="1", type="NUMB"))
-        expected_tree.right = TreeNode(Token(value="*", type="MULT"))
-        expected_tree.right.left = TreeNode(Token(value="3", type="NUMB"))
-        expected_tree.right.right = TreeNode(Token(value="4", type="NUMB"))
+        expected_tree: TreeNode[TokenBase] = TreeNode(Plus())
+        expected_tree.left = TreeNode(Number('1'))
+        expected_tree.right = TreeNode(Mult())
+        expected_tree.right.left = TreeNode(Number('3'))
+        expected_tree.right.right = TreeNode(Number('4'))
 
         assert expected_tree == parse(test_src_tokens)
 
     def test_simple_src_with_reorder(self):
         test_src_tokens = [
-            Token(value="1", type="NUMB"),
-            Token(value="*", type="MULT"),
-            Token(value="3", type="NUMB"),
-            Token(value="+", type="PLUS"),
-            Token(value="4", type="NUMB"),
+            Number('1'),
+            Mult(),
+            Number('3'),
+            Plus(),
+            Number('4'),
         ]
 
-        expected_tree = TreeNode(Token(value="+", type="PLUS"))
-        expected_tree.left = TreeNode(Token(value="*", type="MULT"))
-        expected_tree.left.left = TreeNode(Token(value="1", type="NUMB"))
-        expected_tree.left.right = TreeNode(Token(value="3", type="NUMB"))
-        expected_tree.right = TreeNode(Token(value="4", type="NUMB"))
+        expected_tree: TreeNode[TokenBase] = TreeNode(Plus())
+        expected_tree.left = TreeNode(Mult())
+        expected_tree.left.left = TreeNode(Number('1'))
+        expected_tree.left.right = TreeNode(Number('3'))
+        expected_tree.right = TreeNode(Number('4'))
 
         assert expected_tree == parse(test_src_tokens)
 
     def test_complex_src(self):
         test_src_tokens = [
-            Token(value="1", type="NUMB"),
-            Token(value="+", type="PLUS"),
-            Token(value="3", type="NUMB"),
-            Token(value="*", type="MULT"),
-            Token(value="2", type="NUMB"),
-            Token(value="-", type="MINUS"),
-            Token(value="5", type="NUMB"),
-            Token(value="/", type="DIV"),
-            Token(value="4", type="NUMB"),
-            Token(value="+", type="PLUS"),
-            Token(value="7", type="NUMB"),
+            Number('1'),
+            Plus(),
+            Number('3'),
+            Mult(),
+            Number('2'),
+            Minus(),
+            Number('5'),
+            Div(),
+            Number('4'),
+            Plus(),
+            Number('7'),
         ]
 
-        expected_tree = TreeNode(Token("+", "PLUS"))
-        expected_tree.left = TreeNode(Token("-", "MINUS"))
-        expected_tree.left.left = TreeNode(Token("+", "PLUS"))
-        expected_tree.left.left.left = TreeNode(Token("1", "NUMB"))
-        expected_tree.left.left.right = TreeNode(Token("*", "MULT"))
-        expected_tree.left.left.right.left = TreeNode(Token("3", "NUMB"))
-        expected_tree.left.left.right.right = TreeNode(Token("2", "NUMB"))
-        expected_tree.left.right = TreeNode(Token("/", "DIV"))
-        expected_tree.left.right.left = TreeNode(Token("5", "NUMB"))
-        expected_tree.left.right.right = TreeNode(Token("4", "NUMB"))
-        expected_tree.right = TreeNode(Token("7", "NUMB"))
+        expected_tree: TreeNode[TokenBase] = TreeNode(Plus())
+        expected_tree.left = TreeNode(Minus())
+        expected_tree.left.left = TreeNode(Plus())
+        expected_tree.left.left.left = TreeNode(Number('1'))
+        expected_tree.left.left.right = TreeNode(Mult())
+        expected_tree.left.left.right.left = TreeNode(Number('3'))
+        expected_tree.left.left.right.right = TreeNode(Number('2'))
+        expected_tree.left.right = TreeNode(Div())
+        expected_tree.left.right.left = TreeNode(Number('5'))
+        expected_tree.left.right.right = TreeNode(Number('4'))
+        expected_tree.right = TreeNode(Number('7'))
 
         assert expected_tree == parse(test_src_tokens)
     
     def test_simple_parens(self):
         test_src_tokens = [
-            Token(value="1", type="NUMB"),
-            Token(value="*", type="MULT"),
-            Token(value="(", type="LPAREN"),
-            Token(value="3", type="NUMB"),
-            Token(value="+", type="PLUS"),
-            Token(value="4", type="NUMB"),
-            Token(value=")", type="RPAREN"),
+            Number('1'),
+            Mult(),
+            LeftParen(),
+            Number('3'),
+            Plus(),
+            Number('4'),
+            RightParen(),
         ]
 
-        expected_tree = TreeNode(Token("*", type="MULT"))
-        expected_tree.left = TreeNode(Token("1", type="NUMB"))
-        expected_tree.right = TreeNode(Token("+", type="PLUS"))
-        expected_tree.right.left = TreeNode(Token("3", type="NUMB"))
-        expected_tree.right.right = TreeNode(Token("4", type="NUMB"))
+        expected_tree: TreeNode[TokenBase] = TreeNode(Mult())
+        expected_tree.left = TreeNode(Number('1'))
+        expected_tree.right = TreeNode(Plus())
+        expected_tree.right.left = TreeNode(Number('3'))
+        expected_tree.right.right = TreeNode(Number('4'))
 
         assert expected_tree == parse(test_src_tokens)
     
     def test_complex_parens(self):
         # (1 + 3) * (2 - (5 / 4 + 7))
         test_src_tokens = [
-            Token(value="(", type="LPAREN"),
-            Token(value="1", type="NUMB"),
-            Token(value="+", type="PLUS"),
-            Token(value="3", type="NUMB"),
-            Token(value=")", type="RPAREN"),
-            Token(value="*", type="MULT"),
-            Token(value="(", type="LPAREN"),
-            Token(value="2", type="NUMB"),
-            Token(value="-", type="MINUS"),
-            Token(value="(", type="LPAREN"),
-            Token(value="5", type="NUMB"),
-            Token(value="/", type="DIV"),
-            Token(value="4", type="NUMB"),
-            Token(value="+", type="PLUS"),
-            Token(value="7", type="NUMB"),
-            Token(value=")", type="RPAREN"),
-            Token(value=")", type="RPAREN"),
+            LeftParen(),
+            Number('1'),
+            Plus(),
+            Number('3'),
+            RightParen(),
+            Mult(),
+            LeftParen(),
+            Number('2'),
+            Minus(),
+            LeftParen(),
+            Number('5'),
+            Div(),
+            Number('4'),
+            Plus(),
+            Number('7'),
+            RightParen(),
+            RightParen(),
         ]
 
-        expected_tree = TreeNode(Token("*", type='MULT'))
-        expected_tree.left = TreeNode(Token("+", type="PLUS"))
-        expected_tree.left.left = TreeNode(Token("1", type="NUMB"))
-        expected_tree.left.right = TreeNode(Token("3", type="NUMB"))
-        expected_tree.right = TreeNode(Token("-", type="MINUS"))
-        expected_tree.right.left = TreeNode(Token("2", type="NUMB"))
-        expected_tree.right.right = TreeNode(Token("+", type="PLUS"))
-        expected_tree.right.right.left = TreeNode(Token("/", type="DIV"))
-        expected_tree.right.right.left.left = TreeNode(Token("5", type="NUMB"))
-        expected_tree.right.right.left.right = TreeNode(Token("4", type="NUMB"))
-        expected_tree.right.right.right = TreeNode(Token("7", type="NUMB"))
+        expected_tree: TreeNode[TokenBase] = TreeNode(Mult())
+        expected_tree.left = TreeNode(Plus())
+        expected_tree.left.left = TreeNode(Number('1'))
+        expected_tree.left.right = TreeNode(Number('3'))
+        expected_tree.right = TreeNode(Minus())
+        expected_tree.right.left = TreeNode(Number('2'))
+        expected_tree.right.right = TreeNode(Plus())
+        expected_tree.right.right.left = TreeNode(Div())
+        expected_tree.right.right.left.left = TreeNode(Number('5'))
+        expected_tree.right.right.left.right = TreeNode(Number('4'))
+        expected_tree.right.right.right = TreeNode(Number('7'))
 
         assert expected_tree == parse(test_src_tokens)
     
     def test_single_negation(self):
         # -3
         test_src_tokens = [
-            Token('-', 'MINUS'),
-            Token('3', 'NUMB')
+            Minus(),
+            Number('3')
         ]
 
-        expected_tree = TreeNode(Token('3', 'NUMB'))
-        expected_tree.left = TreeNode(Token('-', 'MINUS'))
+        expected_tree: TreeNode[TokenBase] = TreeNode(Number('3'))
+        expected_tree.left = TreeNode(Minus())
 
         assert expected_tree == parse(test_src_tokens)
     
     def test_simple_negation_with_no_reorder(self):
         # 1 + -3 * 4
         test_src_tokens = [
-            Token("1", "NUMB"),
-            Token("+", "PLUS"),
-            Token("-", "MINUS"),
-            Token("3", "NUMB"),
-            Token("*", "MULT"),
-            Token("4", "NUMB"),
+            Number('1'),
+            Plus(),
+            Minus(),
+            Number('3'),
+            Mult(),
+            Number('4'),
         ]
 
-        expected_tree = TreeNode(Token("+", "PLUS"))
-        expected_tree.left = TreeNode(Token("1", "NUMB"))
-        expected_tree.right = TreeNode(Token("*", "MULT"))
-        expected_tree.right.left = TreeNode(Token("3", "NUMB"))
-        expected_tree.right.left.left = TreeNode(Token("-", "MINUS"))
-        expected_tree.right.right = TreeNode(Token("4", "NUMB"))
+        expected_tree: TreeNode[TokenBase] = TreeNode(Plus())
+        expected_tree.left = TreeNode(Number('1'))
+        expected_tree.right = TreeNode(Mult())
+        expected_tree.right.left = TreeNode(Number('3'))
+        expected_tree.right.left.left = TreeNode(Minus())
+        expected_tree.right.right = TreeNode(Number('4'))
 
         assert expected_tree == parse(test_src_tokens)
     
     def test_simple_negation_with_reorder(self):
         # 1 * -3 + 4
         test_src_tokens = [
-            Token("1", "NUMB"),
-            Token("*", "MULT"),
-            Token("-", "MINUS"),
-            Token("3", "NUMB"),
-            Token("+", "PLUS"),
-            Token("4", "NUMB"),
+            Number('1'),
+            Mult(),
+            Minus(),
+            Number('3'),
+            Plus(),
+            Number('4'),
         ]
 
-        expected_tree = TreeNode(Token("+", "PLUS"))
-        expected_tree.left = TreeNode(Token("*", "MULT"))
-        expected_tree.left.left = TreeNode(Token("1", "NUMB"))
-        expected_tree.left.right = TreeNode(Token("3", "NUMB"))
-        expected_tree.left.right.left = TreeNode(Token("-", "MINUS"))
-        expected_tree.right = TreeNode(Token("4", "NUMB"))
+        expected_tree: TreeNode[TokenBase] = TreeNode(Plus())
+        expected_tree.left = TreeNode(Mult())
+        expected_tree.left.left = TreeNode(Number('1'))
+        expected_tree.left.right = TreeNode(Number('3'))
+        expected_tree.left.right.left = TreeNode(Minus())
+        expected_tree.right = TreeNode(Number('4'))
 
         assert expected_tree == parse(test_src_tokens)
     
     def test_complex_negation(self):
         # (1 + -3) * -(2 - (-5 / 4 + -7))
         test_src_tokens = [
-            Token("(", "LPAREN"),
-            Token("1", "NUMB"),
-            Token("+", "PLUS"),
-            Token("-", "MINUS"),
-            Token("3", "NUMB"),
-            Token(")", "RPAREN"),
-            Token("*", "MULT"),
-            Token("-", "MINUS"),
-            Token("(", "LPAREN"),
-            Token("2", "NUMB"),
-            Token("-", "MINUS"),
-            Token("(", "LPAREN"),
-            Token("-", "MINUS"),
-            Token("5", "NUMB"),
-            Token("/", "DIV"),
-            Token("4", "NUMB"),
-            Token("+", "PLUS"),
-            Token("-", "MINUS"),
-            Token("7", "NUMB"),
-            Token(")", "RPAREN"),
-            Token(")", "RPAREN"),
+            LeftParen(),
+            Number('1'),
+            Plus(),
+            Minus(),
+            Number('3'),
+            RightParen(),
+            Mult(),
+            Minus(),
+            LeftParen(),
+            Number('2'),
+            Minus(),
+            LeftParen(),
+            Minus(),
+            Number('5'),
+            Div(),
+            Number('4'),
+            Plus(),
+            Minus(),
+            Number('7'),
+            RightParen(),
+            RightParen(),
         ]
 
-        expected_tree = TreeNode(Token("*", 'MULT'))
-        expected_tree.left = TreeNode(Token("+", "PLUS"))
-        expected_tree.left.left = TreeNode(Token("1", "NUMB"))
-        expected_tree.left.right = TreeNode(Token("3", "NUMB"))
-        expected_tree.left.right.left = TreeNode(Token("-", "MINUS"))
-        expected_tree.right = TreeNode(Token("*", "MULT"))
-        expected_tree.right.left = TreeNode(Token("1", "NUMB"))
-        expected_tree.right.left.left = TreeNode(Token("-", "MINUS"))
-        expected_tree.right.right = TreeNode(Token("-", "MINUS"))
-        expected_tree.right.right.left = TreeNode(Token("2", "NUMB"))
-        expected_tree.right.right.right = TreeNode(Token("+", "PLUS"))
-        expected_tree.right.right.right.left = TreeNode(Token("/", "DIV"))
-        expected_tree.right.right.right.left.left = TreeNode(Token("5", "NUMB"))
-        expected_tree.right.right.right.left.left.left = TreeNode(Token("-", "MINUS"))
-        expected_tree.right.right.right.left.right = TreeNode(Token("4", "NUMB"))
-        expected_tree.right.right.right.right = TreeNode(Token("7", "NUMB"))
-        expected_tree.right.right.right.right.left = TreeNode(Token("-", "MINUS"))
+        expected_tree: TreeNode[TokenBase] = TreeNode(Mult())
+        expected_tree.left = TreeNode(Plus())
+        expected_tree.left.left = TreeNode(Number('1'))
+        expected_tree.left.right = TreeNode(Number('3'))
+        expected_tree.left.right.left = TreeNode(Minus())
+        expected_tree.right = TreeNode(Mult())
+        expected_tree.right.left = TreeNode(Number('1'))
+        expected_tree.right.left.left = TreeNode(Minus())
+        expected_tree.right.right = TreeNode(Minus())
+        expected_tree.right.right.left = TreeNode(Number('2'))
+        expected_tree.right.right.right = TreeNode(Plus())
+        expected_tree.right.right.right.left = TreeNode(Div())
+        expected_tree.right.right.right.left.left = TreeNode(Number('5'))
+        expected_tree.right.right.right.left.left.left = TreeNode(Minus())
+        expected_tree.right.right.right.left.right = TreeNode(Number('4'))
+        expected_tree.right.right.right.right = TreeNode(Number('7'))
+        expected_tree.right.right.right.right.left = TreeNode(Minus())
 
         assert expected_tree == parse(test_src_tokens)
