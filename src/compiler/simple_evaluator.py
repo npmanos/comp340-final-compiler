37c1136ee339ee2ecec9c2485d61558295024305
Nick Manos <nmanos@student.bridgew.edu>
2024-04-26 02:43:04 -0400

    Implement evaluate()

--- src/compiler/simple_evaluator.py
+++ src/compiler/simple_evaluator.py
@@ -11,3 +11,14 @@ class EvaluationError(Exception):
 
 def evaluate(srcTree: TreeNode[TokenBase]) -> float:
     ...
+    if isinstance(srcTree.data, Number) and srcTree.right is None:
+        if srcTree.left is not None and isinstance(srcTree.left.data, PrefixOperator):
+            return srcTree.left.data.evaluate(operand=float(srcTree.data))
+        else:
+            return float(srcTree.data)
+    
+    if isinstance(srcTree.data, InfixOperator) and srcTree.left is not None and srcTree.right is not None:
+        return srcTree.data.evaluate(left_operand=evaluate(srcTree.left), right_operand=evaluate(srcTree.right))
+    
+    raise EvaluationError
+        
