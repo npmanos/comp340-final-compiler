888f105049c7a27cb3051035c5a5aa4496f48254
Nick Manos <nmanos@student.bridgew.edu>
2024-05-07 23:14:41 -0400

    Implement baby mode

--- src/main.py
+++ src/main.py
@@ -1,13 +1,45 @@
 # ruff: noqa: F403, F405
 
+import importlib.util
 import os
 import sys
 from traceback import format_exception, format_exception_only
 from typing import NoReturn
 
 from compiler import *
+from compiler.simple_baby import BabySyntaxError
 from compiler.simple_evaluator import EvaluationError
 from compiler.simple_tokenizer import TokenizeError
+from compiler.simple_tokens import NumberType
+
+
+BABY_MODE_ENABLED = True
+
+if BABY_MODE_ENABLED:
+    greeting_str = "Hello baby language.\nEnter baby exp and see what you get.\n"
+    exit_command = "poopoo"
+    goodbye_str = "Now it is time to go poo poo."
+    _bottom_text_left = "enther baby wanguage"
+    _bottom_text_right = "comp 340 baby woop"
+
+    _pt_avail = importlib.util.find_spec("prompt_toolkit") is not None
+    _transl_fmt_open = "<ansigray><i>" if _pt_avail else ""
+    _transl_fmt_close = "</i></ansigray>" if _pt_avail else ""
+
+    def process_input(user_input: str) -> NumberType:
+        math_src = decipher(user_input)
+        print(f"... {_transl_fmt_open}Evaluating as {math_src}{_transl_fmt_close}")
+        return evaluate(parse(tokenize(math_src)))
+else:
+    greeting_str = ""
+    exit_command = "exit"
+    goodbye_str = "Now it is time to exit."
+    _bottom_text_left = "Enter math expression to evaluate"
+    _bottom_text_right = "COMP 340 Math REPL"
+
+    def process_input(user_input: str) -> NumberType:
+        return evaluate(parse(tokenize(user_input)))
+
 
 try:
     from prompt_toolkit import HTML, PromptSession, print_formatted_text
@@ -18,8 +50,7 @@ try:
 
     _width, _ = os.get_terminal_size()
 
-    _bottom_text_left = "Enter math expression to evaluate"
-    _bottom_text_right = f'{"COMP 340 Math REPL":>{_width - len(_bottom_text_left)}}'
+    _bottom_text_right = f"{_bottom_text_right:>{_width - len(_bottom_text_left)}}"
 
     _bottom_text = HTML(f"<b>{_bottom_text_left}{_bottom_text_right}</b>")
 
@@ -31,7 +62,21 @@ try:
 
     _lexer = PythonConsoleLexer()
 
-    def print(*values: object, sep: str | None = " ", end: str | None = ""):
+    def print(*values: object, sep: str | None = " ", end: str | None = "\n"):
+        if sep is None:
+            sep = ""
+
+        if end is None:
+            end = ""
+
+        src_code = sep.join(map(str, values))
+
+        print_formatted_text(HTML(src_code), end=end)
+
+    def print_ans(*values: object, sep: str | None = "", end: str | None = ""):
+        print("<ansibrightgreen>", *values, "</ansibrightgreen>", sep, end)
+
+    def print_err(*values: object, sep: str | None = " ", end: str | None = ""):
         if sep is None:
             sep = ""
 
@@ -43,9 +88,9 @@ try:
         tokens = list(lex(src_code, _lexer))
         print_formatted_text(PygmentsTokens(tokens), end=end)
 except ImportError:
-    pass
+    print_ans = print_err = print
 
-__all__ = ()
+__all__ = []
 
 PROMPT = ">>> "
 
@@ -59,21 +104,25 @@ def clear() -> None:
 
 def repl() -> NoReturn:
     clear()
+    print(greeting_str, end="")
     while True:
         try:
-            user_input: str = input(PROMPT)
+            user_input: str = input(PROMPT).lower()
 
-            if user_input.lower() == "exit":
+            if not user_input or user_input.isspace():
+                continue
+
+            if user_input == exit_command:
                 raise EOFError
         except KeyboardInterrupt:
             continue
         except EOFError:
-            print("Now it is time to exit.")
+            print(goodbye_str)
             sys.exit(0)
         else:
             try:
-                print(evaluate(parse(tokenize(user_input))))
-            except (TokenizeError, EvaluationError) as e:
+                print_ans(process_input(user_input))
+            except (BabySyntaxError, TokenizeError, EvaluationError) as e:
                 e.filename = "<stdin>"
                 e.lineno = 1
                 e.offset = e.offset or 1
@@ -81,9 +130,9 @@ def repl() -> NoReturn:
                 e.end_lineno = 1
                 e.end_offset = e.end_offset or len(user_input) + 1
 
-                print(*format_exception_only(e), sep="", end="")
+                print_err(*format_exception_only(e), sep="", end="")
             except Exception as e:
-                print(*format_exception(e), sep="", end="")
+                print_err(*format_exception(e), sep="", end="")
 
 
 if __name__ == "__main__":
