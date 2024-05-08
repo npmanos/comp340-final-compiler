6a608a2c91954d5ea912b827e4153d5b0ed1a3f6
Nick Manos <nmanos@student.bridgew.edu>
2024-05-07 23:13:08 -0400

    Export decipher function from compiler package

--- src/compiler/__init__.py
+++ src/compiler/__init__.py
@@ -1,5 +1,6 @@
+from .simple_baby import decipher
 from .simple_tokenizer import tokenize
 from .simple_parser import parse
 from .simple_evaluator import evaluate
 
-__all__ = ("tokenize", "parse", "evaluate")
+__all__ = ("decipher", "tokenize", "parse", "evaluate")
