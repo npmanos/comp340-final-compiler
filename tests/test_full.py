133163456f5dc188f06e4d0b2ca226591f9bae18
Nick Manos <nmanos@student.bridgew.edu>
2024-05-07 20:23:24 -0400

    Test full evaluation of baby expressions

--- tests/test_full.py
+++ tests/test_full.py
@@ -1,6 +1,8 @@
+from ._test_constants import BABY_TEST_MAP
 from compiler.simple_evaluator import evaluate
 from compiler.simple_parser import parse
 from compiler.simple_tokenizer import tokenize
+from compiler.simple_baby import decipher
 from pytest import mark
 
 py_eval = eval
@@ -22,3 +24,7 @@ py_eval = eval
 )
 def test_tokenize_parse_evaluate(src_str: str):
     assert py_eval(src_str) == evaluate(parse(tokenize(src_str)))
+
+@mark.parametrize("baby_expr,src_str", BABY_TEST_MAP.items())
+def test_decipher_tokenize_parse_evaluate(baby_expr: str, src_str: str):
+    assert py_eval(src_str) == evaluate(parse(tokenize(decipher(baby_expr))))
